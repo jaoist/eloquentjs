9ea2209061593cef6cc98f67fec1d2094202954c
Justin O'Brien <jaocode@gmail.com>
2019-10-09 01:28:11 -0400

    solved minimum

---
+++ Functions/minimum.js
@@ -0,0 +1,14 @@
+/**
+ * Build a function that takes two arguments and returns the minimum.
+ */
+
+function minimum(a, b) {
+    if (a < b){
+        return a;
+    }
+    else {
+        return b;
+    }
+}
+
+console.log(minimum(10, 20));
