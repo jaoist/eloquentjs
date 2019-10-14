d42aed4011ccae064dca7f4a1ce94dee5b2fd356
jaoist <jaocode@gmail.com>
2019-10-14 00:48:32 -0400

    solved list

--- ObjectsArrays/list.js
+++ ObjectsArrays/list.js
@@ -3,17 +3,17 @@
  * the opposite. Create a recursive version of nth.
  */
 
-/* let list = {
+
+
+let arr = [1, 2, 3, 4]
+
+let list = {
     value: 1, rest: {
         value: 2, rest: {
             value: 3, rest: null
             }
         }
     }
-
-console.log(list)
-*/
-let arr = [1, 2, 3, 4]
 
 function reverseArray(arr) {
     console.log(arr);
@@ -33,10 +33,52 @@ function arrayToList(arr) {
     for (let e of arr) {
         console.log(e)
         list.value = e;
-        list.next = previousList;
+        list.rest = previousList;
         previousList = list;
     }
     return list
 }
 
-console.log("List: " + arrayToList(reverseArray(arr)))
+function listToArray(list) {
+    let arr = [];
+
+    while (list) {
+        arr.push(list.value);
+        var list = list.rest;
+    }
+    return arr
+}
+
+function prepend(elem, list) {
+    elem.rest = list;
+    var list = elem;
+
+    return list;
+}
+
+
+function printListValues(list) {
+    let currentElem = list;
+    
+    while (currentElem){
+        console.log(currentElem.value);
+        currentElem = currentElem.rest;
+    }
+}
+
+function nth(list, n) {
+    console.log(n)
+    if (n === 0) {
+        return list.value
+    }
+    else {
+        return nth(list.rest, n - 1)
+    }
+}
+    
+
+printListValues(list);
+console.log(listToArray(list))
+console.log(list)
+console.log(prepend({value: 0}, list))
+console.log(nth(list, 1))
