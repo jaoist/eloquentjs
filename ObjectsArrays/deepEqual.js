f08ac1ee6efeec7321973f43483c98f02245a63f
jaoist <jaocode@gmail.com>
2019-10-14 23:43:52 -0400

    solved deepEqual

---
+++ ObjectsArrays/deepEqual.js
@@ -0,0 +1,37 @@
+/**
+ * Compare two vaules and return true only if they are the same value or are 
+ * objects with the same properties. 
+ */
+
+obj1 = {value: "test"}
+obj2 = {value: "test"}
+obj3 = {value1: 1, value2: 2}
+
+arr1 = [1,2,3]
+arr2 = [1,2,3]
+arr3 = [2,3,4]
+
+function deepEqual(a, b) {
+    if ((a !== null && typeof a == 'object') && 
+        (b !== null && typeof b == 'object')) {
+            //Compare length of property list
+            if (Object.keys(a).length === Object.keys(b).length) {
+                for (let k of Object.keys(a)) {
+                    return deepEqual(a[k], b[k])
+                }
+            }
+            else {
+                return false;
+            }
+        }
+    else {
+        return (a === b)
+    }
+}
+
+console.log(deepEqual(obj1, obj2));
+console.log(deepEqual(obj1, obj3));
+console.log(deepEqual(arr1, arr2));
+console.log(deepEqual(arr1, arr3));
+console.log(deepEqual(1, null));
+console.log(deepEqual(null, null));
