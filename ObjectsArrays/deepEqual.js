7be5197f6b899a4cb91ee8bcf39afdb2c43d06f0
Justin O'Brien <jaocode@gmail.com>
2019-10-15 20:12:33 -0400

    passed author tests

--- ObjectsArrays/deepEqual.js
+++ ObjectsArrays/deepEqual.js
@@ -3,14 +3,14 @@
  * objects with the same properties. 
  */
 
-obj1 = {value: "test"}
-obj2 = {value: "test"}
-obj3 = {value1: 1, value2: 2}
-obj4 = {value: "x"}
+obj1 = {value: "test"};
+obj2 = {value: "test"};
+obj3 = {value1: 1, value2: 2};
+obj4 = {value: "x"};
 
-arr1 = [1,2,3]
-arr2 = [1,2,3]
-arr3 = [2,3,4]
+arr1 = [1,2,3];
+arr2 = [1,2,3];
+arr3 = [2,3,4];
 
 function deepEqual(a, b) {
     if ((a !== null && typeof a == 'object') &&
@@ -18,7 +18,7 @@ function deepEqual(a, b) {
             //Compare length of property list
             if (Object.keys(a).length === Object.keys(b).length) {
                 for (let k of Object.keys(a)) {
-                    return deepEqual(a[k], b[k])
+                    return deepEqual(a[k], b[k]);
                 }
             }
             else {
@@ -26,7 +26,7 @@ function deepEqual(a, b) {
             }
         }
     else {
-        return (a === b)
+        return (a === b);
     }
 }
 
@@ -37,3 +37,10 @@ console.log(deepEqual(arr1, arr3));
 console.log(deepEqual(1, null));
 console.log(deepEqual(null, null));
 console.log(deepEqual(obj1, obj4));
+
+// Authors tests
+console.log("Authors tests below.");
+let obj = {here: {is: "an"}, object: 2};
+console.log(deepEqual(obj, obj));
+console.log(deepEqual(obj, {here: 1, object: 2}));
+console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
